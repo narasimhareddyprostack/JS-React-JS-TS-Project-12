408e71adc582fba9cf64fe231fb308ed02b8372d
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-03-25 17:33:45 +0530

    okie

---
+++ DOM1/data.js
@@ -0,0 +1,7 @@
+let p_ref =document.getElementById('abc')
+console.log(p_ref)
+
+let cdt=new Date().toLocaleString()
+
+//ref.innerHTML="Good Afternoon"
+p_ref.innerHTML=cdt
---
+++ JSEvents/data.js
@@ -0,0 +1,8 @@
+function dislayMessage(){
+
+    alert("Good Afternoon")
+}
+
+function dispayValue(){
+    alert("GA")
+}
